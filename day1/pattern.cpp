9bc9429b7bdd9f2c8fcc1c8990af82d1fe9eeaed
HDS07 <hs4205170@gmail.com>
2024-06-18 22:42:46 +0530

    another pattern commit

--- day1/pattern.cpp
+++ day1/pattern.cpp
@@ -61,9 +61,21 @@ void pattern3(int n){
         }
         cout<<endl;
     }
-} 
+}
+void pattern4(int n){
+        for(int i=0;i<2*n-1;i++){
+            for(int j=0;j<2*n-1;j++){
+                int top=i;
+                int left=j;
+                int right=(2*n-2)-j;
+                int down=(2*n-2)-i;
+                cout<<(n-min(min(top,down),min(left,right)));
+            }
+        cout<<endl;
+        }
+}
 int main(){
     int n;
     cin >> n;
-    pattern3(n);
+    pattern4(n);
 }
