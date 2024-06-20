483d5b6b5f7d17630e7ba9a7947386f5ae0ac224
HDS07 <hs4205170@gmail.com>
2024-06-20 22:16:39 +0530

    pattern 7

--- day1/pattern.cpp
+++ day1/pattern.cpp
@@ -114,8 +114,24 @@ void pattern6(int n){
             cout<<endl;
         }
 }
+void pattern7(int n){
+        int start=1;
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            start=1;
+        }
+        else{
+            start=0;
+        }
+        for(int j=0;j<=i;j++){
+            cout<<start<<" ";
+            start=1-start;
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cin >> n;
-    pattern6(n);
+    pattern7(n);
 }
