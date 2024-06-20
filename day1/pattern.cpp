1099721209522d778dcdfe23baf2b480119cb1c2
HDS07 <hs4205170@gmail.com>
2024-06-20 22:18:30 +0530

    pattern 8 commit

--- day1/pattern.cpp
+++ day1/pattern.cpp
@@ -130,8 +130,34 @@ void pattern7(int n){
         cout<<endl;
     }
 }
+void pattern8(int n){
+            for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                cout<<" ";
+            }
+            for(int k=1;k<=(2*i-1);k++){
+                cout<<"*";
+            }
+            for(int j=1;j<=(n-i);j++){
+                cout<<" ";
+            }
+            cout<<endl;
+	}
+        for(int i=n;i>=1;i--){
+            for(int j=n;j>i;j--){
+                cout<<" ";
+            }
+            for(int k=1;k<=(2*i-1);k++){
+                cout<<"*";
+            }
+            for(int j=n;j>i;j--){
+                cout<<" ";
+            }
+            cout<<endl;
+	}
+}
 int main(){
     int n;
     cin >> n;
-    pattern7(n);
+    pattern8(n);
 }
