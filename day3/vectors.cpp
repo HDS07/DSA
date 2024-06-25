8adb27964b555e1ed74ac8c63c1bd707e4fbcf6f
HDS07 <hs4205170@gmail.com>
2024-06-25 23:04:56 +0530

    vectors learning

---
+++ day3/vectors.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v;
+    v.push_back(11);
+    v.emplace_back(22);
+    cout<<v[1]<<endl;
+    vector<int>v1(5,100);
+    cout<<v1[1]<<endl;
+    v1.push_back(200);
+    cout<<v1[5];
+} 
