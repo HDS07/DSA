efd255fda92f79411c55e5c45903c23c71476914
HDS07 <hs4205170@gmail.com>
2024-07-08 22:39:41 +0530

    day 8 code

---
+++ recursionproblem/III.cpp
@@ -0,0 +1,17 @@
+/*Factoiral no. */
+
+#include<bits/stdc++.h>
+using namespace std;
+int fact(int n){
+    if(n==0){
+        return 1;
+    }
+    else{
+        return n*fact(n-1);
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<"THe factorial of "<<n<<" is "<<fact(n);
+}
