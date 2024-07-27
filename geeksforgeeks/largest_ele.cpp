4f2930ca1bc7273fe02aef0d0e41ee7194fb9164
HDS07 <hs4205170@gmail.com>
2024-07-27 21:52:58 +0530

    largest element of an array

---
+++ geeksforgeeks/largest_ele.cpp
@@ -0,0 +1,32 @@
+// Given an array arr, the task is to find the largest element in it.
+// Examples:
+
+// Input: arr= [1, 8, 7, 56, 90]
+// Output: 90
+// Explanation: The largest element of given array is 90.
+
+#include<bits/stdc++.h>
+using namespace std;
+ int largest(int arr[],int n){
+    int max = arr[0];
+    for (int i=0;i<n;i++){
+        if(max<arr[i]){
+            max = arr[i];
+        }
+    }
+    return max;
+ }
+ int main(){
+    cout<<"Enter the size of an arrray."<<endl;
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout <<"Largest element : " <<largest(arr,n) << endl;
+ }
