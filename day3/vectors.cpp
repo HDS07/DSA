b31d40ee7bc95e7cd6f6c741597552ff605268a1
HDS07 <hs4205170@gmail.com>
2024-12-16 01:31:09 +0530

    revise basics STL for restart DSA

--- day3/vectors.cpp
+++ day3/vectors.cpp
@@ -8,7 +8,28 @@ int main(){
     cout<<v[1]<<endl;
 
     vector<int>v1(5,100);
-    cout<<v1[1]<<endl;
+    cout<< v1.size()<<endl;
+    for(int i=0;i<v1.size();i++){
+        cout<<v1[i]<<" ";
+    }
+    cout<<endl;
     v1.push_back(200);
-    cout<<v1[5];
+    cout<<v1.size()<<endl;
+    cout<<v1.capacity()<<endl;
+    
+
+    /*Vactor increase its capacity automatically as double of its previous capacity when the container is overrfilled*/
+    vector<int>v2;
+    cout<<v2.capacity()<<endl;
+    v2.push_back(1);
+    cout<<v2.capacity()<<endl;
+    v2.push_back(2);
+    cout<<v2.capacity()<<endl;
+    v2.push_back(3);
+    cout<<v2.capacity()<<endl;
+    v2.push_back(4);
+    cout<<v2.capacity()<<endl;
+    v2.push_back(5);
+    cout<<v2.capacity()<<endl;
+    
 } 
